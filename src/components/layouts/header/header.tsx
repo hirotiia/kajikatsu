1762c1bb8db1bc22547705865208d265561a2d07
Hiroya Nakano <hiroya_0610_15mro@me.com>
2024-10-27 18:07:51 +0900

    :sparkles: ヘッダーコンポーネントをDrawerコンポーネントに分割

--- src/components/layouts/header/header.tsx
+++ src/components/layouts/header/header.tsx
@@ -4,8 +4,8 @@ import { Search } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { useEffect, useRef, useState } from 'react';
 
+import { Drawer, DrawerContent, DrawerTrigger } from '@/components/ui/drawer';
 import { config } from '@/config/config';
 import { cn } from '@/utils/cn';
 
@@ -29,19 +29,6 @@ const Logo = () => {
 };
 
 export const Header = ({ className }: HeaderProps) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const headerRef = useRef<HTMLElement | null>(null);
-  useEffect(() => {
-    console.log(headerRef.current);
-    if (headerRef.current) {
-      const contentHeight = headerRef.current.scrollHeight + 'px';
-      headerRef.current.style.setProperty(
-        '--accordion-content-height',
-        contentHeight,
-      );
-      console.log('CSS変数 --accordion-content-height の値:', contentHeight);
-    }
-  }, [isOpen]);
   const pathname = usePathname();
   const applicationPage = [
     '/dashboard',
@@ -63,80 +50,57 @@ export const Header = ({ className }: HeaderProps) => {
     },
   ];
 
-  const toggleAccordion = () => setIsOpen((prev) => !prev);
   return (
     <header
-      ref={headerRef}
       className={cn(
         'flex items-center justify-between border-b border-base-foreground p-4',
         className,
       )}
     >
       <Logo />
-      <button
-        type="button"
-        aria-controls="hamburger-menu"
-        aria-expanded={isOpen}
-        className="relative flex h-10 w-12 items-center justify-center md:hidden"
-        onClick={toggleAccordion}
-      >
-        <span className="sr-only">メニュー</span>
-        {isOpen ? (
-          <span className="relative inline-block size-6 before:absolute before:left-1/2 before:top-1/2 before:block before:h-0.5 before:w-6 before:-translate-x-1/2 before:-translate-y-1/2 before:rotate-45 before:bg-foreground before:transition-transform before:duration-300 after:absolute after:left-1/2 after:top-1/2 after:block after:h-0.5 after:w-6 after:-translate-x-1/2 after:-translate-y-1/2 after:-rotate-45 after:bg-foreground after:transition-transform after:duration-300"></span>
-        ) : (
-          <span
-            className={
-              'relative block h-0.5 w-6 bg-foreground transition duration-300 ease-in-out before:absolute before:h-0.5 before:w-6 before:-translate-y-2 before:bg-foreground before:transition before:duration-300 before:ease-in-out after:absolute after:h-0.5 after:w-6 after:translate-y-2 after:bg-foreground after:transition after:duration-300 after:ease-in-out'
-            }
-          ></span>
-        )}
-      </button>
-      <div
-        className={cn(
-          'overflow-hidden max-md:absolute max-md:top-[72px] max-md:w-full max-md:bg-white max-md:left-0 max-md:right-0 gap-3 md:flex max-md:min-h-[500px]',
-          isOpen
-            ? 'block animate-accordion-down'
-            : 'hidden animate-accordion-up',
-        )}
-        id="hamburger-menu"
-      >
-        {isAppPage && (
-          <form action="/dummy">
-            <label className="relative">
-              <button className="absolute left-1 top-1/2 block -translate-y-1/2">
-                <Search size={20} className="text-primary" />
-              </button>
-              <input
-                type="text"
-                className="border border-primary bg-base p-2 pl-8"
-                placeholder="タスクを検索"
-              />
-            </label>
-          </form>
-        )}
-        <nav className="flex items-center justify-center">
-          <ul className="flex gap-3">
-            {globalNavigation.map(({ title, url }, i) => {
-              const isCurrent = pathname === url;
-              const isLastItem = i + 1 === globalNavigation.length;
-              return (
-                <li key={title}>
-                  <Link
-                    href={url}
-                    className={cn(
-                      'text-lg transition-colors duration-300 ease-in-out hover:text-primary font-bold',
-                      isCurrent && 'text-primary',
-                      isLastItem ? 'py-4 pl-4' : 'p-4',
-                    )}
-                  >
-                    {title}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
-      </div>
+      <Drawer name="hamburger-menu">
+        <DrawerTrigger className="relative flex h-10 w-12 items-center justify-center md:hidden">
+          <span className="sr-only">メニュー</span>
+        </DrawerTrigger>
+        <DrawerContent className="gap-3 overflow-hidden max-md:absolute max-md:inset-x-0 max-md:top-[72px] max-md:min-h-[500px] max-md:w-full max-md:bg-white md:flex">
+          {isAppPage && (
+            <form action="/dummy">
+              <label className="relative">
+                <button className="absolute left-1 top-1/2 block -translate-y-1/2">
+                  <Search size={20} className="text-primary" />
+                </button>
+                <input
+                  type="text"
+                  className="border border-primary bg-base p-2 pl-8"
+                  placeholder="タスクを検索"
+                />
+              </label>
+            </form>
+          )}
+          <nav className="flex items-center justify-center">
+            <ul className="flex gap-3">
+              {globalNavigation.map(({ title, url }, i) => {
+                const isCurrent = pathname === url;
+                const isLastItem = i + 1 === globalNavigation.length;
+                return (
+                  <li key={title}>
+                    <Link
+                      href={url}
+                      className={cn(
+                        'text-lg transition-colors duration-300 ease-in-out hover:text-primary font-bold',
+                        isCurrent && 'text-primary',
+                        isLastItem ? 'py-4 pl-4' : 'p-4',
+                      )}
+                    >
+                      {title}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </DrawerContent>
+      </Drawer>
     </header>
   );
 };
--- src/components/ui/drawer/drawer.tsx
+++ src/components/ui/drawer/drawer.tsx
@@ -1,8 +1,28 @@
-// import { cva } from 'class-variance-authority';
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+
+import { cn } from '@/utils/cn';
+
+type DrawerContextType = {
+  name: string;
+  state: [boolean, Dispatch<SetStateAction<boolean>>];
+};
+
+const DrawerContext = createContext<DrawerContextType>({
+  name: '',
+  state: [false, () => {}],
+});
 
 /**
  * Drawer: 状態を管理
- * DrawerTrigger: ボタンを押下するとDrawerContentを表示非表示制御する
+ * DrawerTrigger: ボタンを押下するとDrawerContentを表示非表示制御するコンポーネントを２つ受け取り、開閉の時に表示を切り替える
  * DrawerContent: DrawerTriggerに制御されるコンテンツ、左右前後で位置指定もできる
  * DrawerCloseTrigger: ボタンを押下すると閉じる
  */
@@ -23,10 +43,89 @@
 //   },
 // );
 
-export const DrawerTrigger = () => {
+const Drawer = ({
+  children,
+  name,
+}: {
+  children: React.ReactNode;
+  name: string;
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    if (contentRef.current) {
+      const contentHeight = contentRef.current.scrollHeight + 'px';
+      contentRef.current.style.setProperty(
+        '--accordion-content-height',
+        contentHeight,
+      );
+    }
+  }, [isOpen]);
+
   return (
-    <div className="">
-      <div className=""></div>
+    <DrawerContext.Provider value={{ name, state: [isOpen, setIsOpen] }}>
+      <div ref={contentRef}>{children}</div>
+    </DrawerContext.Provider>
+  );
+};
+
+const DrawerTrigger = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className: string;
+}) => {
+  const {
+    name,
+    state: [isOpen, setIsOpen],
+  } = useContext(DrawerContext);
+  const toggleAccordion = () => setIsOpen((prev) => !prev);
+  return (
+    <button
+      type="button"
+      aria-expanded={'false'}
+      className={cn('block', className)}
+      aria-controls={name}
+      onClick={toggleAccordion}
+    >
+      <span>{children}</span>
+      {isOpen ? (
+        <span className="relative inline-block size-6 before:absolute before:left-1/2 before:top-1/2 before:block before:h-0.5 before:w-6 before:-translate-x-1/2 before:-translate-y-1/2 before:rotate-45 before:bg-foreground before:transition-transform before:duration-300 after:absolute after:left-1/2 after:top-1/2 after:block after:h-0.5 after:w-6 after:-translate-x-1/2 after:-translate-y-1/2 after:-rotate-45 after:bg-foreground after:transition-transform after:duration-300"></span>
+      ) : (
+        <span
+          className={
+            'relative block h-0.5 w-6 bg-foreground transition duration-300 ease-in-out before:absolute before:h-0.5 before:w-6 before:-translate-y-2 before:bg-foreground before:transition before:duration-300 before:ease-in-out after:absolute after:h-0.5 after:w-6 after:translate-y-2 after:bg-foreground after:transition after:duration-300 after:ease-in-out'
+          }
+        ></span>
+      )}
+    </button>
+  );
+};
+
+const DrawerContent = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className: string;
+}) => {
+  const {
+    name,
+    state: [isOpen],
+  } = useContext(DrawerContext);
+  return (
+    <div
+      id={name}
+      className={cn(
+        'block',
+        isOpen ? 'block animate-accordion-down' : 'hidden animate-accordion-up',
+        className,
+      )}
+    >
+      {children}
     </div>
   );
 };
+
+export { Drawer, DrawerTrigger, DrawerContent };
