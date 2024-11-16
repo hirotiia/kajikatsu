9c37ab8676ce90e5826a893c6bec4288f2b07f11
Hiroya Nakano <hiroya_0610_15mro@me.com>
2024-11-16 14:17:21 +0900

    :zap: 通知コンポーネントのTODO追加

--- src/components/ui/notifications/notifications-store.tsx
+++ src/components/ui/notifications/notifications-store.tsx
@@ -1,1 +1,8 @@
+/**
+ * 通知コンポーネントの状態をグローバルで管理し、外部から通知コンポーネントを作成または削除する機能を提供する
+ * @returns notificaitons 現在表示するすべての通知コンポーネントの配列
+ * @returns addNotification notifications配列にデータを格納
+ * @returns onClose 対象のidを持つコンポーネントを非表示にして、notificationsから削除する
+ */
+
 export const NotificationsStore = () => {};
