387db602c87d14b1ec2a93c250810048ebf7d5e6
Hiroya Nakano <hiroya_0610_15mro@me.com>
2024-11-28 12:38:07 +0900

    :zap: スキーマ更新

---
+++ supabase/migrations/20241128032535_remote_schema.sql
@@ -0,0 +1,45 @@
+grant delete on table "storage"."s3_multipart_uploads" to "postgres";
+
+grant insert on table "storage"."s3_multipart_uploads" to "postgres";
+
+grant references on table "storage"."s3_multipart_uploads" to "postgres";
+
+grant select on table "storage"."s3_multipart_uploads" to "postgres";
+
+grant trigger on table "storage"."s3_multipart_uploads" to "postgres";
+
+grant truncate on table "storage"."s3_multipart_uploads" to "postgres";
+
+grant update on table "storage"."s3_multipart_uploads" to "postgres";
+
+grant delete on table "storage"."s3_multipart_uploads_parts" to "postgres";
+
+grant insert on table "storage"."s3_multipart_uploads_parts" to "postgres";
+
+grant references on table "storage"."s3_multipart_uploads_parts" to "postgres";
+
+grant select on table "storage"."s3_multipart_uploads_parts" to "postgres";
+
+grant trigger on table "storage"."s3_multipart_uploads_parts" to "postgres";
+
+grant truncate on table "storage"."s3_multipart_uploads_parts" to "postgres";
+
+grant update on table "storage"."s3_multipart_uploads_parts" to "postgres";
+
+create policy "Insert avatars (owner only)."
+on "storage"."objects"
+as permissive
+for insert
+to authenticated
+with check (((bucket_id = 'avatars'::text) AND (auth.uid() = owner)));
+
+
+create policy "Select avatars (owner only)."
+on "storage"."objects"
+as permissive
+for select
+to authenticated
+using (((bucket_id = 'avatars'::text) AND (auth.uid() = owner)));
+
+
+
