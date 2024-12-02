fb4504ee23c0e52a90c107cab965f78c0286a83b
Hiroya Nakano <hiroya_0610_15mro@me.com>
2024-12-02 21:35:46 +0900

    :sparkles: dialogコンポーネント作成

---
+++ src/components/ui/dialog/dialog.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import { Plus } from 'lucide-react';
+import React, {
+  useContext,
+  useState,
+  createContext,
+  SetStateAction,
+  Dispatch,
+} from 'react';
+
+import { cn } from '@/utils/cn';
+
+type DialogContextType = {
+  isOpen: boolean;
+  openDialog: Dispatch<SetStateAction<boolean>>;
+  closeDialog: Dispatch<SetStateAction<boolean>>;
+};
+
+type DialogProps = {
+  children: React.ReactNode;
+};
+type DialogContentProps = {
+  children: React.ReactNode;
+};
+type DialogTriggerProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+const DialogContext = createContext<DialogContextType>({
+  isOpen: false,
+  openDialog: () => {},
+  closeDialog: () => {},
+});
+const Dialog = ({ children }: DialogProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const openDialog = () => {
+    setIsOpen(true);
+  };
+  const closeDialog = () => {
+    setIsOpen(false);
+  };
+  return (
+    <DialogContext.Provider value={{ isOpen, openDialog, closeDialog }}>
+      {children}
+    </DialogContext.Provider>
+  );
+};
+
+const DialogContent = ({ children }: DialogContentProps) => {
+  const { isOpen, openDialog, closeDialog } = useContext(DialogContext);
+  console.log(isOpen);
+  console.log(openDialog);
+  console.log(closeDialog);
+  return <dialog className="">{children}</dialog>;
+};
+
+const DialogTrriger = ({ children, className }: DialogTriggerProps) => {
+  return (
+    <button
+      type="button"
+      className={cn('flex gap-4 items-center justify-center', className)}
+    >
+      <Plus />
+      {children}
+    </button>
+  );
+};
+export { Dialog, DialogContent, DialogTrriger };
