1ad277e3ef16fd025509e3102f2b944fbe8c8b44
Hiroya Nakano <hiroya_0610_15mro@me.com>
2024-12-04 12:54:59 +0900

    :sparkles: グループ作成機能追加

--- src/actions/group/create-group.ts
+++ src/actions/group/create-group.ts
@@ -7,14 +7,85 @@ export const createGroup = async (
   formData: FormData,
 ): Promise<any | null> => {
   const supabase = await createClient();
-  const groupData = {
+  const insertData = {
     name: formData.get('group') as string,
   };
 
-  const { error } = await supabase.from('groups').insert([groupData]);
+  const {
+    data: { user },
+    error: authError,
+  } = await supabase.auth.getUser();
+
+  if (authError) {
+    return {
+      type: 'error',
+      status: authError.code,
+      message: authError.message,
+    };
+  }
+
+  // すでに何かしらのグループに入っているか確認する
+  const user_id = user?.id;
+  if (!user_id) {
+    return {
+      type: 'error',
+      status: 'error',
+      message: 'ユーザー情報の取得に失敗しました。',
+    };
+  }
+  const { count, error } = await supabase
+    .from('user_groups')
+    .select('*', { count: 'exact', head: true })
+    .eq('user_id', user_id);
 
   if (error) {
-    return { type: 'error', status: error.code, message: error.message };
+    return {
+      type: 'error',
+      status: error.code,
+      message: error.message,
+    };
+  }
+
+  if (count && count > 0) {
+    return {
+      type: 'warning',
+      status: '失敗',
+      message: 'すでにグループに入っています。',
+    };
+  }
+
+  // グループを作成する
+  const { data, error: createGroupError } = await supabase
+    .from('groups')
+    .insert([insertData])
+    .select();
+
+  if (createGroupError) {
+    return {
+      type: 'error',
+      status: createGroupError.code,
+      message: createGroupError.message,
+    };
+  }
+
+  const group_id = data[0]?.id;
+
+  const insertRelationData = {
+    user_id,
+    group_id,
+    role_id: 'owner',
+  };
+
+  const { error: setDataError } = await supabase
+    .from('user_groups')
+    .insert([insertRelationData]);
+
+  if (setDataError) {
+    return {
+      type: 'error',
+      status: setDataError.code,
+      message: setDataError.message,
+    };
   }
 
   return {
