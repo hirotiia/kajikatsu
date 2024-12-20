f7c23e3574650204f9d93fbdd27f2c0f0c0efd8f
Hiroya Nakano <hiroya_0610_15mro@me.com>
2024-12-20 12:13:08 +0900

    :sparkles: Tabコンポーネントヘッダー作成

--- src/components/ui/tab/tab.tsx
+++ src/components/ui/tab/tab.tsx
@@ -30,6 +30,8 @@ type TabProps = {
 
 type TabContextState = {
   currentKey: TabKey;
+  setCurrentKey: React.Dispatch<React.SetStateAction<TabKey>>;
+  tabList: TabHeader[];
 };
 
 type TabHeader = {
@@ -39,59 +41,124 @@ type TabHeader = {
 
 const TabContext = createContext<TabContextState>({
   currentKey: '',
+  setCurrentKey: () => {},
+  tabList: [],
 });
-
-const TabItem = ({ children, tabKey, className }: TabItemProps) => {
-  const { currentKey } = useContext(TabContext);
-
-  if (currentKey !== tabKey) return null;
-
-  return <div className={cn('w-full bg-black', className)}>{children}</div>;
-};
-TabItem.displayName = 'TabItem';
 
 const Tab = ({ children, defaultKey, className }: TabProps) => {
   const [currentKey, setCurrentKey] = useState(defaultKey);
-
-  const handleChangeTab = (event: ChangeEvent<HTMLSelectElement>) => {
-    setCurrentKey(event.target.value);
-  };
-
   const tabList = useMemo<TabHeader[]>(() => {
     const items = React.Children.toArray(
       children,
     ) as ReactElement<TabItemProps>[];
-    return items.map((child) => {
-      if ((child.type as any).displayName !== 'TabItem') {
-        throw new Error('TabItemを利用してください');
-      }
-      return { tabKey: child.props.tabKey, label: child.props.label };
-    });
+    return items
+      .map((child) => {
+        if (
+          (child.type as any).displayName === 'TabSelectHeader' ||
+          (child.type as any).displayName === 'TabHeader'
+        ) {
+          return undefined;
+        }
+        if ((child.type as any).displayName !== 'TabItem') {
+          throw new Error('TabItemを利用してください');
+        }
+        return { tabKey: child.props.tabKey, label: child.props.label };
+      })
+      .filter((tab): tab is TabHeader => tab !== undefined);
   }, [children]);
 
   return (
-    <TabContext.Provider value={{ currentKey }}>
-      <div className={cn('', className)}>
-        <div className="">
-          <label>
-            <select
-              name="members"
-              onChange={handleChangeTab}
-              value={currentKey}
-            >
-              {tabList.map(({ tabKey, label }) => (
-                <option key={tabKey} value={tabKey}>
-                  {label}
-                </option>
-              ))}
-            </select>
-          </label>
-          さんの家事タスク一覧
-        </div>
+    <TabContext.Provider value={{ currentKey, setCurrentKey, tabList }}>
+      <div
+        className={cn(
+          'w-full rounded-lg border border-muted shadow-lg',
+          className,
+        )}
+      >
         {children}
       </div>
     </TabContext.Provider>
   );
 };
 
-export { Tab, TabItem };
+const TabHeader = ({ className }: { className?: string }) => {
+  const { currentKey, setCurrentKey, tabList } = useContext(TabContext);
+
+  return (
+    <ul className={cn('flex gap-4 border-b-1 border-muted pb-2', className)}>
+      {tabList.map(({ tabKey, label }) => {
+        return (
+          <li className="" key={tabKey}>
+            <button
+              type="button"
+              className={cn(
+                'px-4 py-2 rounded-lg',
+                tabKey === currentKey
+                  ? 'bg-primary text-primary-foreground'
+                  : 'bg-muted text-foreground hover:bg-base',
+              )}
+              onClick={() => {
+                setCurrentKey(tabKey);
+              }}
+            >
+              {label}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+TabHeader.displayName = 'TabHeader';
+
+type TabSelectHeaderProps = {
+  children?: React.ReactNode;
+  className?: string;
+};
+
+const TabSelectHeader = ({ children, className }: TabSelectHeaderProps) => {
+  const { currentKey, setCurrentKey, tabList } = useContext(TabContext);
+
+  const handleChangeTab = (event: ChangeEvent<HTMLSelectElement>) => {
+    setCurrentKey(event.target.value);
+  };
+
+  return (
+    <div className={cn('flex flex-col gap-2', className)}>
+      <label className="text-sm text-muted">
+        <select
+          name="members"
+          onChange={handleChangeTab}
+          value={currentKey}
+          className="rounded border border-muted bg-base p-2 text-foreground"
+        >
+          {tabList.map(({ tabKey, label }) => (
+            <option key={tabKey} value={tabKey}>
+              {label}
+            </option>
+          ))}
+        </select>
+      </label>
+      {children}
+    </div>
+  );
+};
+
+TabSelectHeader.displayName = 'TabSelectHeader';
+
+const TabItem = ({ children, tabKey, className }: TabItemProps) => {
+  const { currentKey } = useContext(TabContext);
+
+  if (currentKey !== tabKey) return null;
+
+  return (
+    <div className={cn('w-full p-4 bg-base-foreground text-base', className)}>
+      {children}
+    </div>
+  );
+};
+
+TabItem.displayName = 'TabItem';
+
+export { Tab, TabHeader, TabSelectHeader, TabItem };
--- src/features/todos/components/tab/tab-users-task.tsx
+++ src/features/todos/components/tab/tab-users-task.tsx
@@ -1,10 +1,19 @@
 'use client';
 
-import { Tab, TabItem } from '@/components/ui/tab';
+import { Tab, TabHeader, TabItem } from '@/components/ui/tab';
+
+/**
+ * グループメンバーがいない場合
+ * 自身のタスクを取得する
+ * グループメンバーがいる場合
+ * groupInfoからグループメンバーを取得する
+ * taskResultからグループごとのタスク内容を取得する
+ */
 
 export const TabUsersTask = () => {
   return (
     <Tab defaultKey="test1">
+      <TabHeader />
       <TabItem tabKey="test1" label="test1">
         <p className="text-base">test1</p>
       </TabItem>
