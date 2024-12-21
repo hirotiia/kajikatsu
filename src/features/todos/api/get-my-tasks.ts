808e5e9e857fef3f59fbdcf981500b3d9fe3ce26
Hiroya Nakano <hiroya_0610_15mro@me.com>
2024-12-21 13:26:07 +0900

    :zap: 不要な記述を削除

--- src/features/todos/api/get-my-tasks.ts
+++ src/features/todos/api/get-my-tasks.ts
@@ -4,27 +4,13 @@ import useSWR from 'swr';
 
 import { MyTasksResponse } from '@/lib/supabase/data/tasks/select/get-my-tasks';
 
-const fetcher = (url: string) => {
-  console.log('----------fetcher called with URL------------');
-  console.log(`Fetcher called with URL: ${url}`);
-  console.log('----------fetcher called with URL------------');
-
-  return fetch(url).then((res) => res.json());
-};
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export const useMyTasks = () => {
   const { data, error, isLoading } = useSWR<MyTasksResponse>(
     '/api/get/get-my-tasks',
     fetcher,
   );
-
-  console.log('------------error-------------');
-  console.log(error);
-  console.log('------------error-------------');
-
-  console.log('------------data-------------');
-  console.log(data);
-  console.log('------------data-------------');
 
   return {
     myTasks: data,
