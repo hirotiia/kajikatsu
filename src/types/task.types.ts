8e97d17289b98e8ca6a86ee1da5e002f83759740
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-01-19 18:35:50 +0900

    :sparkles: タスクの型定義ファイル作成

---
+++ src/types/task.types.ts
@@ -0,0 +1,10 @@
+export type Task = {
+  id: string;
+  title: string;
+  description: string | null;
+  statusId: string | null;
+  statusName: string | null;
+  createdAt: string | null;
+  updatedAt: string | null;
+  expiresAt: string | null;
+};
