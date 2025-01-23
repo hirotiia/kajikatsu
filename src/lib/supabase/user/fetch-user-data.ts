5b74d17624114e19e7d816bf78aad8518ae38b25
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-01-23 17:27:29 +0900

    :sparkles: ユーザーのデータを取得する関数作成

---
+++ src/lib/supabase/user/fetch-user-data.ts
@@ -0,0 +1,65 @@
+import { createClient } from '@/lib/supabase/server';
+import { UserState } from '@/types/user-state.types';
+
+// TODO: 最終的にはユーザー情報の取得はこのファイルのみにする。
+/**
+ * 指定したユーザーIDに基づいてユーザーデータとグループ情報を取得する。
+ * @param userId ユーザーID
+ * @returns State 型に一致するオブジェクト
+ */
+export async function getUserData(userId: string): Promise<UserState | null> {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase
+    .from('user_groups')
+    .select(
+      `
+        user_id,
+        users ( username, avatar_url ),
+        group_id,
+        groups ( name ),
+        role_id,
+        roles ( id, name )
+      `,
+    )
+    .eq('user_id', userId)
+    .single();
+
+  if (error || !data) {
+    // グループ未所属のユーザー情報を取得
+    const { data: userData, error: userError } = await supabase
+      .from('users')
+      .select('username, avatar_url')
+      .eq('id', userId)
+      .single();
+
+    // ユーザー情報も存在しない場合は null を返却
+    if (userError || !userData) {
+      return null;
+    }
+
+    return {
+      id: userId,
+      username: userData?.username || '',
+      avatar_url: userData?.avatar_url || null,
+      group: null,
+    };
+  }
+
+  return {
+    id: data.user_id,
+    username: data.users?.username || '',
+    avatar_url: data.users?.avatar_url || null,
+    group: data.group_id
+      ? {
+          id: data.group_id,
+          name: data.groups?.name || '',
+          role: {
+            id: data.roles?.id || '',
+            name:
+              (data.roles?.name as 'viewer' | 'editor' | 'admin') || 'viewer',
+          },
+        }
+      : null,
+  };
+}
