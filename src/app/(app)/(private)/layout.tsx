fa649ac980760e9ea5ff3777d1be95114120c7cb
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-02-27 10:13:01 +0900

    :zap: リダイレクト設定

--- src/app/(app)/(private)/layout.tsx
+++ src/app/(app)/(private)/layout.tsx
@@ -1,17 +1,24 @@
 import { Metadata } from 'next';
+import { redirect } from 'next/navigation';
 
 import { PrivateLayout } from '@/components/layouts/private-layout';
 import { config } from '@/config/config';
+import { isAuthenticated } from '@/lib/supabase/user/is-authenticated';
 
 export const metadata: Metadata = {
   title: config.APP_NAME,
   description: '家事負荷分担アプリ”カジ活”',
 };
 
-export default function ApplicationLayout({
+export default async function ApplicationLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const authGuard: boolean = await isAuthenticated();
+
+  if (!authGuard) {
+    redirect('/login');
+  }
   return <PrivateLayout>{children}</PrivateLayout>;
 }
