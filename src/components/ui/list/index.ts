0e9844db80bf2bdc81d5f9d418c3baa1b9dd26e3
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-03-18 17:01:12 +0900

    :sparkles: ニュースリストコンポーネント設計

--- src/components/ui/list/index.ts
+++ src/components/ui/list/index.ts
@@ -1,2 +1,3 @@
 export * from './list';
 export * from './definition-list';
+export * from './news-list';
---
+++ src/components/ui/list/news-list.tsx
@@ -0,0 +1,57 @@
+import { Frown } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { cn } from '@/utils/cn';
+
+type YYYYMMDD =
+  `${number}${number}${number}${number}-${number}${number}-${number}${number}`;
+
+type NewsListItem = {
+  key: string;
+  avatarUrl?: string;
+  date: YYYYMMDD;
+  title: string;
+  link: string;
+};
+
+type NewsListProps = {
+  items: NewsListItem[];
+  className?: string;
+};
+
+export const NewsList = ({ items = [], className }: NewsListProps) => {
+  return (
+    <ol className={cn('grid gap-y-3', className)}>
+      {items.map(({ key, avatarUrl, date, link, title }: NewsListItem) => (
+        <li key={key} className="border-b border-foreground pb-3">
+          <div className="flex gap-x-3 md:items-center">
+            <div className="shrink-0 overflow-hidden rounded-full">
+              {avatarUrl ? (
+                <Image
+                  src={avatarUrl}
+                  alt="ユーザーアバター"
+                  width={30}
+                  height={30}
+                />
+              ) : (
+                <Frown size={30}>アイコン未登録のユーザー</Frown>
+              )}
+            </div>
+            <div className="md:flex md:gap-x-3">
+              <div className="">
+                <time dateTime={date}>{date}</time>
+              </div>
+              <Link
+                href={link}
+                className="custom-transition hover:text-primary"
+              >
+                <b>{title}</b>
+              </Link>
+            </div>
+          </div>
+        </li>
+      ))}
+    </ol>
+  );
+};
