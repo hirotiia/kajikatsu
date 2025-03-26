06b9bb165a7a767f5e393dab9345c61eb8c4c842
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-03-26 16:31:58 +0900

    :zap: 型定義を一元管理

--- src/types/task.types.ts
+++ src/types/task.types.ts
@@ -9,3 +9,88 @@ export type Task = {
   expiresAt: string | null;
   assigneeId?: string | null;
 };
+
+export type TaskHistory = {
+  id: string;
+  task_id: string;
+  action_id: string;
+  changed_by: string;
+  changed_at: string;
+  details: {
+    old: TaskDetails | null;
+    new: TaskDetails | null;
+  };
+};
+
+export type TaskDetails = {
+  id: string;
+  title: string;
+  description: string | '';
+  status_id: string;
+  is_deleted: boolean;
+  created_by: string;
+  created_at: string;
+  updated_by: string;
+  updated_at: string;
+  expires_at: string | null;
+  assignee_id: string | null;
+  group_id: string;
+};
+
+export type EnhancedTaskHistory = {
+  id: string;
+  task: {
+    id: string;
+    title: string;
+  };
+  action: {
+    id: string;
+    name: 'updated' | 'completed' | 'created' | 'deleted' | undefined;
+  };
+  changedBy: {
+    id: string;
+    username: string;
+    avatarUrl: string | null;
+  };
+  changedAt: string;
+  details: {
+    old: EnhancedTaskDetails | null;
+    new: EnhancedTaskDetails | null;
+    changes: Array<{
+      field: string;
+      oldValue: any;
+      newValue: any;
+      fieldLabel: string;
+    }>;
+  };
+};
+
+export type EnhancedTaskDetails = {
+  id: string;
+  title: string;
+  description: string | '';
+  status: {
+    id: string;
+    name: string;
+  };
+  isDeleted: boolean;
+  createdBy: {
+    id: string;
+    username: string;
+  };
+  createdAt: string;
+  updatedBy: {
+    id: string;
+    username: string;
+  };
+  updatedAt: string;
+  expiresAt: string | null;
+  assignee: {
+    id: string;
+    username: string;
+  } | null;
+  group: {
+    id: string;
+    name: string;
+  };
+};
