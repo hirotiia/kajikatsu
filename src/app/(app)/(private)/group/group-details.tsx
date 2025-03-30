e72089ead30d8f3754cee4c3579aa33eadfd8602
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-03-30 13:46:54 +0900

    :recycle: getUser関数ではなくfetchUserDataでデータを取得するように変更

--- src/app/(app)/(private)/group/group-details.tsx
+++ src/app/(app)/(private)/group/group-details.tsx
@@ -3,34 +3,24 @@ import { CreateGroup } from '@/features/group/components/create-group';
 import { DleteGroup } from '@/features/group/components/delete-group';
 import { InviteGroup } from '@/features/group/components/invite-group';
 import { fetchUserData } from '@/lib/supabase/user/fetch-user-data';
-import { getUser } from '@/lib/supabase/user/user';
-import { UserState } from '@/types/user-state.types';
 
 export const GroupDetails = async () => {
-  const { user } = await getUser();
-
-  if (!user) {
-    return <p>ユーザー情報が取得できませんでした。</p>;
+  const data = await fetchUserData();
+  if (!data) {
+    throw new Error('ユーザー情報の取得に失敗しました。');
   }
-  const userState: UserState | null = await fetchUserData(user.id);
-
-  if (!userState) {
-    return <h2>ユーザー情報を取得できませんでした。</h2>;
-  }
-
-  const groupName = userState.group?.name ?? null;
   return (
     <>
       <DefinitionList
         items={[
           {
             term: 'グループ名',
-            definitions: [groupName ?? '未加入'],
+            definitions: [data.group?.name ?? '未加入'],
           },
         ]}
       />
       <div className="mt-6 flex gap-3">
-        {groupName ? (
+        {data.group ? (
           <>
             <InviteGroup />
             <DleteGroup />
