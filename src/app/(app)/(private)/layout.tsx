3ae09c2982dfd74033564629f6b08af45002838c
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-04-03 11:43:30 +0900

    :fire: このブランチでする作業ではないため一旦削除

--- src/app/(app)/(private)/layout.tsx
+++ src/app/(app)/(private)/layout.tsx
@@ -5,7 +5,6 @@ import { PrivateLayout } from '@/components/layouts/private-layout';
 import { config } from '@/config/config';
 import { isAuthenticated } from '@/lib/supabase/user/is-authenticated';
 
-export const dynamic = 'force-dynamic';
 export const metadata: Metadata = {
   title: config.APP_NAME,
   description: '家事負荷分担アプリ”カジ活”',
