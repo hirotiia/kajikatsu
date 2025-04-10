6d38ff65121d6b931ceb19a7d8df9a8f3dac3a22
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-04-10 20:31:35 +0900

    :wrench: storybook他テスティングライブラリの導入 src配下がコミット対象に存在しなかった場合、コマンドエラーが発生していたので、eslintをスキップするように修正 シークレットリントの検証範囲を全ファイルから、コミット対象のファイルに修正

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,15 +1,29 @@
 import path from 'path';
 
 const buildEslintCommand = (filenames) => {
-  return `next lint --no-cache --fix --file ${filenames
+  const filteredFiles = filenames
     .filter((f) => f.includes('/src/'))
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`;
+    .map((f) => path.relative(process.cwd(), f));
+
+  // srcディレクトリ内のファイルがない場合はスキップ
+  if (filteredFiles.length === 0) {
+    return 'echo "src/ 配下の対象ファイルが存在しなかったため、ESlintをスキップします。"';
+  }
+
+  return `next lint --no-cache --fix --file ${filteredFiles.join(' --file ')}`;
+};
+
+const buildSecretlintCommand = (filenames) => {
+  if (filenames.length === 0) {
+    return 'echo "シークレットリントのチェック対象ファイルはありません。"';
+  }
+
+  return `npx secretlint ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`;
 };
 
 const config = {
   '*.{ts,tsx}': [buildEslintCommand, "bash -c 'npm run check-types'"],
-  '**/*': 'npm run secretlint',
+  '**/*': buildSecretlintCommand,
 };
 
 export default config;
