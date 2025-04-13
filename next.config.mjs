aabe3254ed53444f8412ca14292da8aa54e49670
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-04-13 17:48:10 +0900

    :wrench: httpsでの通信を強制する

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
