1ca704d8f59a8b3b34178caf1a67d0ff0a0edfee
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-04-13 18:50:09 +0900

    :wrench: インラインスタイルを許可しないように修正。 nonceが設定された場合は、許可する

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ export function middleware(request: NextRequest) {
   const cspHeader = `
     default-src 'self';
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-    style-src 'self' 'unsafe-inline' 'nonce-${nonce}';
+    style-src 'self' 'nonce-${nonce}';
     img-src 'self' blob: data: http://127.0.0.1:54321 https://${process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID}.supabase.co;
     connect-src 'self' https://${process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID}.supabase.co wss://${process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID}.supabase.co;
     font-src 'self';
