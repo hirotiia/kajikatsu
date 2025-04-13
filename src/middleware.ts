6041306b219072645ea4d0fc76c32f2ac2ce1251
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-04-13 18:19:18 +0900

    :wrench: websocketからのレスポンスも許可する

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,7 @@ export function middleware(request: NextRequest) {
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
     style-src 'self' 'unsafe-inline' 'nonce-${nonce}';
     img-src 'self' blob: data: http://127.0.0.1:54321 https://${process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID}.supabase.co;
-    connect-src 'self' https://${process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID}.supabase.co;
+    connect-src 'self' https://${process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID}.supabase.co wss://${process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID}.supabase.co;
     font-src 'self';
     object-src 'none';
     base-uri 'self';
