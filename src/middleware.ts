3e658c2c25535b447c34300ddb1740de929eca03
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-04-15 11:02:23 +0900

    :wrench: static-dynamicを削除しても結局nonceを付与する必要があるようなので、戻す

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ export function middleware(request: NextRequest) {
   const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}';
+    script-src 'self' 'nonce-${nonce}' 'static-dynamic;
     style-src 'self' 'nonce-${nonce}';
     img-src 'self' blob: data: http://127.0.0.1:54321 https://${process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID}.supabase.co;
     connect-src 'self' https://${process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID}.supabase.co wss://${process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID}.supabase.co;
@@ -33,6 +33,7 @@ export function middleware(request: NextRequest) {
       headers: requestHeaders,
     },
   });
+
   response.headers.set(
     'Content-Security-Policy',
     contentSecurityPolicyHeaderValue,
