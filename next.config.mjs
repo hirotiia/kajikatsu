954157f806230a4263d326b0ff1e7ee7a3dba4b3
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-04-15 11:35:15 +0900

    :wrench: 現状app routerではNext.js が自動生成するすべてのスクリプトタグにnonce を適用するのが難しいため、nonceの設定はやめる

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,27 @@
 /** @type {import('next').NextConfig} */
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
+
 const nextConfig = {
   async headers() {
     return [
       {
         source: '/(.*)',
         headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
           {
             key: 'Strict-Transport-Security',
             value: 'max-age=63072000; includeSubDomains; preload',
