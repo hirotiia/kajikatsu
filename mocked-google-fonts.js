4fe097bf94eb20177df9a599a99e9a4997c99962
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-05-04 21:56:01 +0900

    :wrench: VRTでフォントの読み込みエラーで見た目の変更が発生しないようにフォントをモックしておく

---
+++ mocked-google-fonts.js
@@ -0,0 +1,19 @@
+module.exports = {
+  'https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic:wght@400;700&display=swap': `
+    /* モックフォント：Zen Maru Gothic */
+    @font-face {
+      font-family: 'Zen Maru Gothic';
+      font-style: normal;
+      font-weight: 400;
+      font-display: swap;
+      src: url(data:font/woff2;base64,d09GMgABAAAAAAnYAB...) format('woff2');
+    }
+    @font-face {
+      font-family: 'Zen Maru Gothic';
+      font-style: normal;
+      font-weight: 700;
+      font-display: swap;
+      src: url(data:font/woff2;base64,d09GMgABAAAABaABAB...) format('woff2');
+    }
+  `,
+};
