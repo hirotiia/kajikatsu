8bd0befb04d629199cb9427208f5d3c151878d8a
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-05-09 17:14:08 +0900

    :wrench: CDN経由でフォントをモックする

--- mocked-google-fonts.js
+++ mocked-google-fonts.js
@@ -1,18 +1,63 @@
 const mockedFonts = {
   'https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic:wght@400;700&display=swap': `
+    /* ラテン文字 (英数字) */
     @font-face {
       font-family: 'Zen Maru Gothic';
       font-style: normal;
       font-weight: 400;
       font-display: swap;
-      src: url(data:font/woff2;base64,d09GMgABAAAAAAnYAB...) format('woff2');
+      src: url(https://fonts.gstatic.com/s/zenmarugothic/v17/o-0SIpIxzW5b-RxT-6A8jWAtCp-k6EJgNLDc4aPt1DLTVkMY-EVqJJo7qCKeuXNZ.2.woff2) format('woff2');
+      unicode-range: U+ffd7, U+ffda-ffdc, U+ffe0-ffe2, U+ffe4, U+ffe6, U+ffe8-ffee, U+1f100-1f10c, U+1f110-1f16c, U+1f170-1f1ac, U+1f200-1f202, U+1f210-1f234;
     }
+
+    /* ひらがな */
+    @font-face {
+      font-family: 'Zen Maru Gothic';
+      font-style: normal;
+      font-weight: 400;
+      font-display: swap;
+      src: url(https://fonts.gstatic.com/s/zenmarugothic/v17/o-0SIpIxzW5b-RxT-6A8jWAtCp-k6EJgNLDc4aPt1DLTVkMY-EVqJJo7qCKeuXNZ.3.woff2) format('woff2');
+      unicode-range: U+fa10, U+fa12-fa6d, U+fb00-fb04, U+fe10-fe19, U+fe30-fe42, U+fe44-fe52, U+fe54-fe66, U+fe68-fe6b, U+ff02, U+ff04, U+ff07, U+ff51, U+ff5b, U+ff5d, U+ff5f-ff60, U+ff66, U+ff69, U+ff87, U+ffa1-ffbe, U+ffc2-ffc7, U+ffca-ffcf, U+ffd2-ffd6;
+    }
+
+    /* カタカナ・漢字など */
+    @font-face {
+      font-family: 'Zen Maru Gothic';
+      font-style: normal;
+      font-weight: 400;
+      font-display: swap;
+      src: url(https://fonts.gstatic.com/s/zenmarugothic/v17/o-0SIpIxzW5b-RxT-6A8jWAtCp-k6EJgNLDc4aPt1DLTVkMY-EVqJJo7qCKeuXNZ.4.woff2) format('woff2');
+      unicode-range: U+f92d-f959, U+f95b-f9f2, U+f9f4-fa0b, U+fa0e-fa0f;
+    }
+
+    /* ラテン文字 (英数字) */
     @font-face {
       font-family: 'Zen Maru Gothic';
       font-style: normal;
       font-weight: 700;
       font-display: swap;
-      src: url(data:font/woff2;base64,d09GMgABAAAABaABAB...) format('woff2');
+      src: url(https://fonts.gstatic.com/s/zenmarugothic/v17/o-0XIpIxzW5b-RxT-6A8jWAtCp-cUW1COZqc_am_qlmzXUwN6k5QM5oCrTKUrBY1uqOu.2.woff2) format('woff2');
+      unicode-range: U+ffd7, U+ffda-ffdc, U+ffe0-ffe2, U+ffe4, U+ffe6, U+ffe8-ffee, U+1f100-1f10c, U+1f110-1f16c, U+1f170-1f1ac, U+1f200-1f202, U+1f210-1f234;
+    }
+
+    /* ひらがな */
+    @font-face {
+      font-family: 'Zen Maru Gothic';
+      font-style: normal;
+      font-weight: 700;
+      font-display: swap;
+      src: url(https://fonts.gstatic.com/s/zenmarugothic/v17/o-0XIpIxzW5b-RxT-6A8jWAtCp-cUW1COZqc_am_qlmzXUwN6k5QM5oCrTKUrBY1uqOu.3.woff2) format('woff2');
+      unicode-range: U+fa10, U+fa12-fa6d, U+fb00-fb04, U+fe10-fe19, U+fe30-fe42, U+fe44-fe52, U+fe54-fe66, U+fe68-fe6b, U+ff02, U+ff04, U+ff07, U+ff51, U+ff5b, U+ff5d, U+ff5f-ff60, U+ff66, U+ff69, U+ff87, U+ffa1-ffbe, U+ffc2-ffc7, U+ffca-ffcf, U+ffd2-ffd6;
+    }
+
+    /* カタカナ・漢字など */
+    @font-face {
+      font-family: 'Zen Maru Gothic';
+      font-style: normal;
+      font-weight: 700;
+      font-display: swap;
+      src: url(https://fonts.gstatic.com/s/zenmarugothic/v17/o-0XIpIxzW5b-RxT-6A8jWAtCp-cUW1COZqc_am_qlmzXUwN6k5QM5oCrTKUrBY1uqOu.4.woff2) format('woff2');
+      unicode-range: U+f92d-f959, U+f95b-f9f2, U+f9f4-fa0b, U+fa0e-fa0f;
     }
   `,
 };
