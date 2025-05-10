aeb84e06989041c7d6d56eb9de320eccd91c5744
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-05-10 12:58:21 +0900

    :sparkles: boxのストーリーズ作成

--- src/components/ui/box/box.stories.tsx
+++ src/components/ui/box/box.stories.tsx
@@ -11,6 +11,11 @@ export const Default = {
     children:
       'これはダミーテキストです。この文章は見た目を確認するために使われています。実際の文章が入る前に、デザインやレイアウトを確認するための仮のテキストです。改行や行間、フォントサイズの調整に役立ちます。',
     className: '',
+    border: 'default',
+    bg: 'default',
+    color: 'default',
+    rounded: 'default',
+    mt: 'md',
   },
 };
 
