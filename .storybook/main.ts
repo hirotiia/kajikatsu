cc44b0136f9b2df813d723dafe3f8bfd596297f5
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-05-11 22:52:22 +0900

    :wrench: storybookアクセシビリティアドオンを追加

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,6 +11,7 @@ const config: StorybookConfig = {
     '@storybook/addon-onboarding',
     '@chromatic-com/storybook',
     '@storybook/experimental-addon-test',
+    '@storybook/addon-a11y',
   ],
   framework: {
     name: '@storybook/experimental-nextjs-vite',
