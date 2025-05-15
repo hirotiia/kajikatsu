bf1cf9c7572f08e2da327c8a0d7f230ff96488fc
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-05-15 19:58:14 +0900

    :zap: meta定義不要な設定を削除

--- src/components/ui/button/button.stories.tsx
+++ src/components/ui/button/button.stories.tsx
@@ -3,26 +3,8 @@ import { Meta, StoryObj } from '@storybook/react';
 import { Button } from './index';
 
 const meta: Meta<typeof Button> = {
-  component: Button,
   title: 'Ui/Button',
-  tags: ['autodocs'],
-  argTypes: {
-    variant: {
-      control: 'select',
-      default: 'default',
-      options: ['default', 'login', 'destructive'],
-    },
-    size: {
-      control: 'select',
-      default: 'default',
-      options: ['default', 'full', 'small', 'auto'],
-    },
-    rounded: {
-      control: 'select',
-      default: 'sm',
-      options: ['sm', 'md'],
-    },
-  },
+  component: Button,
 };
 
 export default meta;
