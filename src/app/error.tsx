a73ceb8a6596da7eacfdccfcdb75557427d82d29
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-05-20 13:25:08 +0900

    :sparkles: エラー専用ページ作成

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,12 +1,32 @@
 'use client';
 
-import Link from 'next/link';
+import { Metadata } from 'next';
+
+import { Button } from '@/components/ui/button';
+import { Heading } from '@/components/ui/heading';
+import { Text } from '@/components/ui/text';
+import { config } from '@/config/config';
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: `エラー｜${config.APP_NAME}`,
+  };
+}
 
 export default function Error({ error }: { error: Error }) {
   return (
-    <div>
-      <p>{error.message}</p>
-      <Link href="/">TOPページへ戻る</Link>
+    <div className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <Text spacing="none">500</Text>
+        <Heading as="h1">予期しないエラーが発生しました</Heading>
+        <Text>申し訳ありません。ページの表示中に問題が発生しました。</Text>
+        <Text>詳細：{error.message}</Text>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Button as="a" href="/" size="small">
+            ホームに戻る
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
