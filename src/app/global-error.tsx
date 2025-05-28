265f48c5d6917ffd71ef6b662e010cb952d83d03
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-05-28 15:11:57 +0900

    :wrench: エラー監視

---
+++ src/app/global-error.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import * as Sentry from '@sentry/nextjs';
+import { Metadata } from 'next';
+import { useEffect } from 'react';
+
+import { Button } from '@/components/ui/button';
+import { Heading } from '@/components/ui/heading';
+import { Text } from '@/components/ui/text';
+import { config } from '@/config/config';
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: `エラー｜${config.APP_NAME}`,
+  };
+}
+export default function GlobalError({
+  error,
+}: {
+  error: Error & { digest?: string };
+}) {
+  useEffect(() => {
+    Sentry.captureException(error);
+  }, [error]);
+
+  return (
+    <div className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <Text spacing="none">500</Text>
+        <Heading as="h1" className="text-center">
+          予期しないエラーが発生しました
+        </Heading>
+        <Text>申し訳ありません。ページの表示中に問題が発生しました。</Text>
+        <Text>詳細：{error.message}</Text>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Button as="a" href="/" size="small" rounded="md">
+            ホームに戻る
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
