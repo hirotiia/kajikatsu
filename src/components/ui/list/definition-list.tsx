5ccb2f7bcb8aba286766d4af944a04954897da7b
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-05-29 15:30:35 +0900

    :zap: 定義リストコンポーネントのマークアップ修正

--- src/components/ui/list/definition-list.tsx
+++ src/components/ui/list/definition-list.tsx
@@ -1,29 +1,16 @@
 import { cva, VariantProps } from 'class-variance-authority';
-import React, { JSX } from 'react';
+import { JSX } from 'react';
 
 import { cn } from '@/utils/cn';
 
-const dlStyles = cva('grid gap-y-4');
-
+// Base styles
+const dlStyles = cva('grid gap-y-2');
 const itemStyles = cva(
   'grid grid-cols-[85px_1fr] gap-x-1 md:grid-cols-[100px_1fr] md:gap-x-4',
 );
 
-const dtStyles = cva('font-semibold', {
-  variants: {
-    textSize: {
-      sm: 'text-sm md:text-base',
-      lg: 'text-lg',
-    },
-  },
-  defaultVariants: {
-    textSize: 'sm',
-  },
-});
-
-const ddContainerStyles = cva('grid gap-y-2');
-
-const ddStyles = cva('', {
+// Variant for text size
+const textSizeVariants = cva('', {
   variants: {
     textSize: {
       sm: 'text-sm md:text-base',
@@ -40,19 +27,13 @@ export type DefinitionListItem = {
   definitions: (string | JSX.Element)[];
 };
 
-type TextSizeVariant = VariantProps<typeof dtStyles>['textSize'];
-
 export type DefinitionListProps = {
   items: DefinitionListItem[];
   className?: string;
-  textSize?: TextSizeVariant;
-};
+} & VariantProps<typeof textSizeVariants>;
 
 /**
  * DefinitionList コンポーネント
- *
- * 各項目が横並びになり、複数の定義がある場合は縦に積み重なる
- * dtとddは横並びで、複数のddは縦積みになる
  */
 export function DefinitionList({
   items,
@@ -68,18 +49,17 @@ export function DefinitionList({
 
         return (
           <div key={term} className={cn(itemStyles())}>
-            <dt className={cn(dtStyles({ textSize }))}>{term}：</dt>
-
-            <div className={cn(ddContainerStyles())}>
-              {definitions.map((definition, idx) => (
-                <dd
-                  key={`${term}-${idx}`}
-                  className={cn(ddStyles({ textSize }))}
-                >
-                  {definition}
-                </dd>
-              ))}
-            </div>
+            <dt className={cn('font-semibold', textSizeVariants({ textSize }))}>
+              {term}：
+            </dt>
+            {definitions.map((definition, idx) => (
+              <dd
+                key={`${term}-${idx}`}
+                className={cn(textSizeVariants({ textSize }))}
+              >
+                {definition}
+              </dd>
+            ))}
           </div>
         );
       })}
