e468e9e2f4bc6a691b07eab746ba8ffac7de4b5b
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-05-30 14:55:32 +0900

    :wrench: 監視項目を制限

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -4,25 +4,16 @@
 
 import * as Sentry from '@sentry/nextjs';
 
+const IS_PROD = process.env.NEXT_PUBLIC_ENV === 'production';
+
 Sentry.init({
   dsn: 'https://ad4d7de4716a75bbc610362ccd127445@o4509398321332224.ingest.us.sentry.io/4509398322577409',
-
-  // 追加機能のために、オプションのインテグレーションを追加します
   // eslint-disable-next-line import/namespace
-  integrations: [Sentry.replayIntegration()],
-  // トレースがサンプリングされる確率を定義します。
-  // 本番環境ではこの値を調整するか、より詳細に制御したい場合は tracesSampler を使用してください。
-  tracesSampleRate: 1,
-
-  // Replay イベントがどのくらいの確率でサンプリングされるかを定義します。
-  // この設定では本番環境の場合、サンプル率を 10%（10人に一人） にしています。
-  replaysSessionSampleRate: 0.1,
-
-  // エラーが発生したときに Replay イベントがどのくらいの確率でサンプリングされるかを定義します。
-  replaysOnErrorSampleRate: 1.0,
-
-  // このオプションを true に設定すると、Sentry のセットアップ中に有用な情報がコンソールに出力されます。
-  debug: false,
+  integrations: IS_PROD ? [] : [Sentry.replayIntegration()],
+  tracesSampleRate: IS_PROD ? 0.1 : 1.0,
+  replaysSessionSampleRate: IS_PROD ? 0.0 : 0.1,
+  replaysOnErrorSampleRate: IS_PROD ? 1.0 : 1.0,
+  debug: !IS_PROD,
 });
 
 // eslint-disable-next-line import/namespace
