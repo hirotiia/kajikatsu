2b4e4ee5ca35608783c87687e197daa6e5c06de3
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-05-31 14:42:57 +0900

    Revert ":recycle: パフォーマンステスト"
    This reverts commit 02cb286906a526e73e6144232d3d28d54009b4d4.

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,20 +1,32 @@
 'use client';
 
-import { Provider } from 'react-redux';
+import { useEffect } from 'react';
+import { Provider, useDispatch } from 'react-redux';
 
 import { Notifications } from '@/components/ui/notifications';
-import { store } from '@/stores/index';
+import { AppDispatch, store } from '@/stores/index';
+import { fetchAsyncUserData } from '@/stores/user/reducer';
 
 type AppProviderProps = {
   children: React.ReactNode;
 };
+
+function AppProviderInner({ children }: { children: React.ReactNode }) {
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatch(fetchAsyncUserData());
+  }, [dispatch]);
+
+  return <>{children}</>;
+}
 
 export const AppProvider = ({ children }: AppProviderProps) => {
   return (
     <>
       <Provider store={store}>
         <Notifications />
-        {children}
+        <AppProviderInner>{children}</AppProviderInner>
       </Provider>
     </>
   );
