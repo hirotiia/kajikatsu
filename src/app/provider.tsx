02cb286906a526e73e6144232d3d28d54009b4d4
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-05-31 14:06:43 +0900

    :recycle: パフォーマンステスト

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,32 +1,20 @@
 'use client';
 
-import { useEffect } from 'react';
-import { Provider, useDispatch } from 'react-redux';
+import { Provider } from 'react-redux';
 
 import { Notifications } from '@/components/ui/notifications';
-import { AppDispatch, store } from '@/stores/index';
-import { fetchAsyncUserData } from '@/stores/user/reducer';
+import { store } from '@/stores/index';
 
 type AppProviderProps = {
   children: React.ReactNode;
 };
-
-function AppProviderInner({ children }: { children: React.ReactNode }) {
-  const dispatch = useDispatch<AppDispatch>();
-
-  useEffect(() => {
-    dispatch(fetchAsyncUserData());
-  }, [dispatch]);
-
-  return <>{children}</>;
-}
 
 export const AppProvider = ({ children }: AppProviderProps) => {
   return (
     <>
       <Provider store={store}>
         <Notifications />
-        <AppProviderInner>{children}</AppProviderInner>
+        {children}
       </Provider>
     </>
   );
