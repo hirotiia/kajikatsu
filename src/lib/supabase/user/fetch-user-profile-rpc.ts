c050c36572a7eea6db18f0ea978cf99ccdd593db
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-06-01 13:19:29 +0900

    :truck: ファイル名変更

---
+++ src/lib/supabase/user/fetch-user-profile-rpc.ts
@@ -0,0 +1,37 @@
+import * as Sentry from '@sentry/nextjs';
+
+import { createClient } from '@/lib/supabase/server';
+import { FunctionReturn } from '@/types/supabase/database.types';
+
+export type UserProfile = FunctionReturn<'get_user_profile'>;
+
+/**
+ * 指定したユーザーIDに基づいてユーザーデータとグループ情報を取得する。
+ * userId が指定されていない場合は、現在ログイン中のユーザーIDを利用する。
+ * @param userId ユーザーID(任意)
+ * @returns State 型に一致するオブジェクト
+ */
+export async function fetchUserProfileRpc(): Promise<UserProfile | null> {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.rpc('get_user_profile', {});
+
+  if (error) {
+    Sentry.captureException(error, {
+      tags: {
+        feature: 'user-profile',
+        layer: 'server',
+        severity: 'high',
+        env: process.env.NODE_ENV,
+      },
+      extra: {
+        location: 'fetchUserProfileRsc',
+        timestamp: new Date().toISOString(),
+      },
+    });
+
+    throw new Error(error.message);
+  }
+
+  return data;
+}
