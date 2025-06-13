0a98a4047ac8bd6313473a3201910c27f2cfe624
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-06-13 19:26:08 +0900

    :sparkles: thコンポーネント作成

---
+++ src/components/ui/table/th.tsx
@@ -0,0 +1,58 @@
+import { ComponentPropsWithoutRef, PropsWithChildren, useMemo } from 'react';
+import { tv, VariantProps } from 'tailwind-variants';
+
+import { scrollShadowClassNameGenerator } from './table-scroll/styles/scroll-shadow-class-name-generator';
+
+type Props = PropsWithChildren<{
+  fixed?: 'right' | 'left';
+}> &
+  VariantProps<typeof classNameGenerator>;
+type ThElement = Omit<ComponentPropsWithoutRef<'th'>, keyof Props | 'onClick'>;
+
+const classNameGenerator = tv({
+  base: [],
+  variants: {
+    align: {
+      left: '',
+      right: 'text-right',
+    },
+    vAlign: {
+      middle: '',
+      baseline: 'align-baseline',
+      bottom: 'align-bottom',
+    },
+  },
+  defaultVariants: {
+    align: 'left',
+    vAlign: 'middle',
+  },
+});
+
+export const Th = ({
+  children,
+  className,
+  align,
+  vAlign,
+  fixed,
+  ...props
+}: Props & ThElement) => {
+  const actualClassName = useMemo(() => {
+    const base = classNameGenerator({ className, align, vAlign });
+
+    if (!fixed) {
+      return base;
+    }
+
+    const shadow = scrollShadowClassNameGenerator({
+      showShadow: true,
+      direction: fixed,
+    });
+
+    return `${base} ${shadow}`;
+  }, [className, align, vAlign, fixed]);
+  return (
+    <th {...props} className={actualClassName}>
+      {children}
+    </th>
+  );
+};
